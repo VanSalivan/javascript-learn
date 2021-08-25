1e0933ebd9c0977e21e08b2b562eec3156a8c329
aleksandr akelev <aleksandr.akelev@simbirsoft.com>
2021-08-25 15:12:28 +0300

    Практика задач: lastThreeEvenNumber

---
+++ Практика/Практические_задачи/lastThreeEvenNumber.js
@@ -0,0 +1,19 @@
+//Given an array of digital numbers, 
+// return a new array of length number containing the last even numbers from the original array(in the same order).
+// The original array will be not empty and will contain at least "number" even numbers.
+
+//For example:
+
+const lastThreeEvenNumber = (array, number) => {
+  let evenArray = array.reduce((total,item) => {
+    if (item % 2 === 0) total.push(item);
+
+    return total
+  }, [])
+
+  return evenArray.slice(-number)
+}
+
+console.log(lastThreeEvenNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) // [4, 6, 8]
+console.log(lastThreeEvenNumber([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2)) // [-8, 26]
+console.log(lastThreeEvenNumber([6, -25, 3, 7, 5, 5, 7, -3, 23], 1)) // [6]
